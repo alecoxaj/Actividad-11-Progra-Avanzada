666999f30f4fe74bc336f17886b279305eebca9b
Alejandro <alejandrocoxaj94@gmail.com>
2025-07-31 09:40:01 -0600

    Alejandro ya hizo el resumen de los propietarios.

--- Actividad11.py
+++ Actividad11.py
@@ -9,33 +9,41 @@ for i in range(cantidad_propietarios):
     name = input("Ingresa tu nombre: ")
     phone = input("Ingresa tu teléfono: ")
 
+    cantidad_vehículos = int(input("Ingresa la cantidad de vehículos: "))
+    vehicles = {}
 
-cantidad_vehículos = int(input("Ingresa la cantidad de vehículos: "))
-vehicles = {}
+    for j in range(cantidad_vehículos):
+        print(f"Vehículo {j+1}")
+        plate = input("Ingresa tu placa: ")
+        brand = input("Ingresa la marca: ")
+        model = input("Ingresa el modelo: ")
+        year = int(input("Ingresa el año: "))
+        tax_payed = input("¿Pagaste el impuesto? (si/no): ")
 
-for j in range(cantidad_vehículos):
-    print(f"Vehículo {j+1}")
-    plate = input("Ingresa tu placa: ")
-    brand = input("Ingresa la marca: ")
-    model = input("Ingresa el modelo: ")
-    year = int(input("Ingresa el año: "))
-    tax_payed = input("¿Pagaste el impuesto? (si/no): ")
-
-    vehicles[plate] = {
-        "plate": plate,
-        "brand": brand,
-        "model": model,
-        "year": year,
-        "tax": tax_payed,
-    }
-
+        vehicles[plate] = {
+            "plate": plate,
+            "brand": brand,
+            "model": model,
+            "year": year,
+            "tax": tax_payed,
+        }
 
     propietarios[nit] = {
         "name": name,
         "phone ": phone,
-    }
+        "vehicles" : vehicles,
+}
 
 print("\nResumen de propietarios: ")
 for nit, datos in propietarios.items():
     print(f"\nNúmero de NIT: {nit}")
+    print(f"Nombre: {datos['name']}\n")
+    print(f"Teléfono: {datos['phone']}\n")
+    print(f"Vehículo: {datos['vehicles']}")
+    for plate, v in datos["plate"].items():
+        print(f"Placa: {plate} | {v['brand']} {v['model']} | {v['year']} | Impuesto: {v['tax']}")
+
+
+
+
 
